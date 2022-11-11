63b02960ee2b6512cbf77760aaed962ba63db7e6
vikas.h <vikivikas838@gmail.com>
2022-11-11 16:53:55 +0530

    updated to data storing in db

--- Assignment-2/src/main/java/main/Runner.java
+++ Assignment-2/src/main/java/main/Runner.java
@@ -4,16 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import greeting.Greet;
+import Repositories.AccountDAO;
+import entities.Account;
 
 @Component
 public class Runner implements CommandLineRunner {
 	@Autowired
-	private Greet greeter;
+	private AccountDAO accountDAO ;
 
 	@Override
 	public void run(String... args) throws Exception {
-		greeter.greet();
+		Account account1=new Account("Vikas",123456789L,22335L);
+		accountDAO.save(account1);
+		
 	}
 
 }
